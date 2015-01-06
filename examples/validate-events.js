a19b41c6a9ccd36f711fb349d0428b73aa95528a
Grigoriy Chudnov <g.chudnov@gmail.com>
2015-01-07 01:26:48 +0300

    fixed code

--- examples/validate-events.js
+++ examples/validate-events.js
@@ -5,14 +5,14 @@ var validator = require('./../index');
 var href = 'http://google.com';
 
 validator.validate(href)
+  .on('error', function(err) {
+    console.log('ERROR', err);
+  })
   .on('validation-error', function(data) {
     console.log('validation error', data);
   })
   .on('validation-warning', function(data) {
     console.log('validation warning', data);
-  })
-  .on('error', function(err) {
-    console.log('ERROR', err);
   })
   .on('end', function() {
     console.log('validation complete');
